5dc839016dbefa17a23c940f6940050205c725cd
Lior Nitzan <nitzanlior1968@gmail.com>
2022-01-08 23:06:17 +0100

    all countries endpoint returns fallback empty array

--- src/routes/api/countries/allcountriesdata.json.ts
+++ src/routes/api/countries/allcountriesdata.json.ts
@@ -1,15 +1,20 @@
-import { getAllCountriesData } from "../../../utils/data-utils";
+import { getAllCountriesData } from '../../../utils/data-utils';
 import type { country } from '../../../utils/types';
 
-
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get(): Promise<{ body: { countries: country[]; }; }> {
-  const countries = getAllCountriesData()
+export async function get(): Promise<{ body: { countries: country[] } }> {
+	const countries = getAllCountriesData();
 
 	if (countries) {
 		return {
 			body: {
-				countries,
+				countries
+			}
+		};
+	} else {
+		return {
+			body: {
+				countries: []
 			}
 		};
 	}
